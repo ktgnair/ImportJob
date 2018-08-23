9fed671fe372ee5d04eb269125063f93f9f9f046
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-08-23 21:55:21 +0530

    SI: Added: Automated email being generated by Import Job with report file showing specific error message for each row.

--- src/main/java/com/krishagni/importcsv/core/CsvImporter.java
+++ src/main/java/com/krishagni/importcsv/core/CsvImporter.java
@@ -4,12 +4,18 @@ import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.krishagni.catissueplus.core.administrative.domain.User;
 import com.krishagni.catissueplus.core.biospecimen.events.CollectionProtocolRegistrationDetail;
 import com.krishagni.catissueplus.core.biospecimen.events.ParticipantDetail;
 import com.krishagni.catissueplus.core.biospecimen.events.PmiDetail;
@@ -22,11 +28,16 @@ import com.krishagni.catissueplus.core.common.errors.OpenSpecimenException;
 import com.krishagni.catissueplus.core.common.events.RequestEvent;
 import com.krishagni.catissueplus.core.common.events.ResponseEvent;
 import com.krishagni.catissueplus.core.common.util.ConfigUtil;
+import com.krishagni.catissueplus.core.common.util.CsvFileWriter;
+import com.krishagni.catissueplus.core.common.util.EmailUtil;
+import com.krishagni.catissueplus.core.common.util.Utility;
 import com.krishagni.importcsv.datasource.DataSource;
 import com.krishagni.importcsv.datasource.Impl.CsvFileDataSource;
 
 public class CsvImporter {
-	private final static String FILE_NAME = "import.csv";
+	private final static String INPUT_FILE_NAME = "/Users/swapnil/Downloads/apache-tomcat-9.0.10/data/import.csv";
+	
+	private final static String REPORT_FILE_NAME = "/Users/swapnil/Downloads/apache-tomcat-9.0.10/data/csv-import-output.csv";
 	
 	private final static String DATE_FORMAT = "MM/dd/yyyy";
 	
@@ -49,13 +60,21 @@ public class CsvImporter {
 	private final static String VISIT_COMMENTS = "Visit Comments";
 	
 	private final static String DAY = "Day";
+
+	private final static String MSK_IMPORT_JOB_STATUS = "msk_import_job_status";
 	
 	private final static Log logger = LogFactory.getLog(CsvImporter.class);
 	
 	private OpenSpecimenException ose;
 	
 	private DataSource dataSource;
+
+	private CsvFileWriter reportFile;
 	
+	private int rowCount;
+	
+	private int recordsFailed;
+
 	@Autowired
 	private CollectionProtocolRegistrationService cprSvc;
 	
@@ -64,16 +83,24 @@ public class CsvImporter {
 	
 	@PlusTransactional
 	public void importCsv() {
-		ose = new OpenSpecimenException(ErrorType.USER_ERROR);
-		dataSource = new CsvFileDataSource(getFile());
-		
+		ose =  new OpenSpecimenException(ErrorType.USER_ERROR);
+		dataSource = new CsvFileDataSource(INPUT_FILE_NAME);
+		createOutputCsvFile();
+		rowCount = 0;
+		recordsFailed = 0;
+			
 		try {
 		    isHeaderRowValid(dataSource); 
 		    while (dataSource.hasNext()) {
 		    	Record record = dataSource.nextRecord();
 		    	importParticipant(record);
+		    	rowCount++;
 		    }
-		    ose.checkAndThrow();
+		    
+		    if (ose.hasAnyErrors()) {
+		    	sendImportReportMail();
+		    	ose.checkAndThrow();
+		    }
 		} catch (OpenSpecimenException ose) {
 		    logger.error("Error while parsing csv file in: \n" + ose.getMessage());
 		} catch (Exception e) {
@@ -83,18 +110,18 @@ public class CsvImporter {
 		    if (dataSource != null) {
 		    	dataSource.close();
 		    }
+		    IOUtils.closeQuietly(reportFile);
 		}
 	}
 	
-	private String getFile() {
-		File file = new File(ConfigUtil.getInstance().getDataDir() + File.separatorChar, FILE_NAME); 
-		return file.getAbsolutePath();
-	}
+//	private String getFile() {
+//		File file = new File(ConfigUtil.getInstance().getDataDir() + File.separatorChar, FILE_NAME); 
+//		return file.getAbsolutePath();
+//	}
 	
-	private void importParticipant(Record record) throws ParseException {                
+	private void importParticipant(Record record) throws ParseException {
 		CollectionProtocolRegistrationDetail cprDetail = new CollectionProtocolRegistrationDetail();
 		VisitDetail visitDetail = new VisitDetail();
-		
 		cprDetail.setCpShortTitle(record.getValue(CP_SHORT_TITLE));
 		cprDetail.setParticipant(new ParticipantDetail());
 		cprDetail.setRegistrationDate(new SimpleDateFormat(DATE_FORMAT).parse(record.getValue(VISIT_DATE)));
@@ -125,33 +152,108 @@ public class CsvImporter {
 		
 		if (participantResponse.getError() != null) {
 			participantResponse.getError().getErrors().forEach(error -> ose.addError(error.error(), error.params()));
+			addRowToReport(record, participantResponse.getError());
 		}
 		
 		if (visitResponse.getError() != null) {
 			visitResponse.getError().getErrors().forEach(error -> ose.addError(error.error(), error.params()));
+			addRowToReport(record, visitResponse.getError());
+		}
+			
+		if (participantResponse.getError() != null) {
+			participantResponse.getError().getErrors().forEach(error -> ose.addError(error.error(), error.params()));
+			addRowToReport(record, participantResponse.getError());
 		}
 	}
 	
+	private void addRowToReport(Record record, OpenSpecimenException error) {
+		List<String> data = getRow(record);
+		data.add(error.getMessage());
+		reportFile.writeNext(data.toArray(new String[data.size()]));
+		recordsFailed++;
+	}
+
+	private List<String> getRow(Record record) {
+		List<String> headers = getCsvHeaders();
+		List<String> values = new ArrayList<>();
+		headers.forEach(header -> values.add(record.getValue(header)));
+		
+		return values;
+	}
+
 	private void isHeaderRowValid(DataSource dataSource) throws Exception {
 		String[] csvHeaderRow = dataSource.getHeader();
-		List<String> expectedHeader = new ArrayList<String>();
-		
-		expectedHeader.add(FIRST_NAME);
-		expectedHeader.add(LAST_NAME);
-		expectedHeader.add(PPID);
-		expectedHeader.add(MRN);
-		expectedHeader.add(CP_SHORT_TITLE);
-		expectedHeader.add(VISIT_DATE);
-		expectedHeader.add(SITE_NAME);
-		expectedHeader.add(VISIT);
-		expectedHeader.add(DAY);
-		expectedHeader.add(VISIT_COMMENTS);
+		List<String> expectedHeader = getCsvHeaders();
 		
 		for (String header : csvHeaderRow) {
 			if (!expectedHeader.contains(header)) {
 				throw new Exception("Could not parse the file because the headers of the CSV file is not as expected.");
 			}
 		}
+	}
+	
+	private List<String> getCsvHeaders() {
+		List<String> headers = new ArrayList<String>();
+		
+		headers.add(FIRST_NAME);
+		headers.add(LAST_NAME);
+		headers.add(PPID);
+		headers.add(MRN);
+		headers.add(CP_SHORT_TITLE);
+		headers.add(VISIT_DATE);
+		headers.add(SITE_NAME);
+		headers.add(VISIT);
+		headers.add(DAY);
+		headers.add(VISIT_COMMENTS);
+		
+		return headers;
+	}
+	
+	private void sendImportReportMail() {
+		String date = Utility.getDateString(Calendar.getInstance().getTime());
+
+		Map<String, Object> emailProps = new HashMap<>();
+		emailProps.put("$subject", new String[] {date});
+		emailProps.put("date", date);
+		emailProps.put("ccAdmin", false);
+		emailProps.putAll(getMailprops());
+
+		List<User> rcpts = new ArrayList<>();
+
+		String itAdminEmailId = ConfigUtil.getInstance().getItAdminEmailId();
+		if (StringUtils.isNotBlank(itAdminEmailId)) {
+			User itAdmin = new User();
+			itAdmin.setFirstName("IT");
+			itAdmin.setLastName("Admin");
+			itAdmin.setEmailAddress(itAdminEmailId);
+			rcpts.add(itAdmin);
+		}
+
+		String emailTmpl = MSK_IMPORT_JOB_STATUS;
+		File[] attachments = new File[] {new File(REPORT_FILE_NAME)};
+		for (User user : rcpts) {
+			emailProps.put("rcpt", user);
+			EmailUtil.getInstance().sendEmail(emailTmpl, new String[] {user.getEmailAddress()}, attachments, emailProps);
+		}
+	}
+	
+	private Map<String, Object> getMailprops() {
+		Map<String, Object> result = new HashMap<>();
+		
+		result.put("totalRecords", this.rowCount);
+		result.put("failedRecords", this.recordsFailed);
+		result.put("passedRecords", this.rowCount - this.recordsFailed);
+		result.put("jobID", "Not Specified");
+		result.put("filename", INPUT_FILE_NAME);
+		
+		return result;
+	}
+	
+	private void createOutputCsvFile() {
+		reportFile = CsvFileWriter.createCsvFileWriter(new File(REPORT_FILE_NAME));
+		List<String> headers = getCsvHeaders();
+		headers.add("ERROR");
+		reportFile.writeNext(headers.toArray(new String[headers.size()]));
 	}
 	
 	private <T> RequestEvent<T> getRequest(T payload) {
